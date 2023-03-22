d6296115c9aabe4aa652326a1dcab77328505e23
saraalvarex <alvarezalmacen19@gmail.com>
2023-03-22 21:35:54 +0100

    v_alumnos_pagos

---
+++ ProyectoERP/Controllers/AlumnosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoERP.Repositories;
+
+namespace ProyectoERP.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        private IRepo repo;
+        //private HelperPathProvider helperPath;
+        public AlumnosController(IRepo repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
